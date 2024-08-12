9119ddd4f947b11621998a1cc9f74b1dce564b48
Abhinav Tirole <abhinav.tirole2021@vitbhopal.ac.in>
2024-08-12 20:53:32 +0530

    first commit

--- src/screens/auth/Login.jsx
+++ src/screens/auth/Login.jsx
@@ -18,32 +18,32 @@ function Login() {
         <div className="signin-section">
           <h2 style={{textAlign:'center'}}>Sign in</h2>
           <form>
-            <div className="input-field">
+            {/* <div className="input-field">
               <label>User Name</label>
               <input type="text" placeholder="User Name" required />
             </div>
             <div className="input-field">
               <label>Password</label>
               <input type="password" placeholder="Password" required />
-            </div>
+            </div> */}
             <div className="options">
-              <label>
+              {/* <label>
                 <input type="checkbox" />
                 Remember me
-              </label>
-              <a href="#">Forgot Password?</a>
+              </label> */}
+              {/* <a href="#">Forgot Password?</a> */}
             </div>
             <a href={loginEndpoint}>
               <div className="login-btn">Sign in</div>
             </a>
           </form>
-          <div className="or">Or</div>
+          {/* <div className="or">Or</div>
           <a href={loginEndpoint}>
             <div className="login-alt-btn">Sign in with other</div>
           </a>
           <div className="signup-link">
             Don't have an account? <a href="#">Sign Up</a>
-          </div>
+          </div> */}
         </div>
       </div>
     </div>
